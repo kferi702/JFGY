90de4f7aa3de6eb1f69737e9b61e5f875322b268
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-02 12:54:40 +0100

    rétegek megadása és Racer class fejl.

---
+++ MVCDeveloping-master/Forma1/Forma1/Repository/Racer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forma1.Repository
+{
+    public class Racer
+    {
+        private string name;
+        private int age;
+        private int salary;
+
+        public Racer(string name, int age, int salary)
+        {
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+
+        public void setSalary(int salary)
+        {
+            this.salary = salary;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public int getSalary()
+        {
+            return salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} {age} éves és fizetése {salary}";
+        }
+    }
+}
