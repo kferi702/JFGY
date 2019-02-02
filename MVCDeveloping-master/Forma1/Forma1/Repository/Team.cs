2a7f7b8af52e9e51c8b43abd9c9e9e7dd68b6913
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-02 13:02:21 +0100

    Create Team.cs

---
+++ MVCDeveloping-master/Forma1/Forma1/Repository/Team.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forma1.Repository
+{
+    public class Team
+    {
+        private string name;
+        private List<Racer> racers;
+
+        public Team(string name)
+        {
+            this.name = name;
+            racers = new List<Racer>();
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public List<Racer> getRacers()
+        {
+            return racers;
+        }
+
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+}
