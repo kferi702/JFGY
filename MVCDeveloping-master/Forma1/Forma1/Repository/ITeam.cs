4bafd8917e6bc599a656d7fee92b7f39399b1465
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-02 13:12:41 +0100

    ITeam interface létrehozás

---
+++ MVCDeveloping-master/Forma1/Forma1/Repository/ITeam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forma1.Repository
+{
+    public interface ITeam
+    {
+        void Create(string name);
+        void Update(string newName);
+        void Delete();
+
+        int getTeamSalary();
+    }
+}
