70ff907ff0cbb7cb6adbd319bdfb2a553b52fbef
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-02 13:55:46 +0100

    Update TeamManageRacers.cs

--- MVCDeveloping-master/Forma1/Forma1/Repository/TeamManageRacers.cs
+++ MVCDeveloping-master/Forma1/Forma1/Repository/TeamManageRacers.cs
@@ -8,16 +8,32 @@ namespace Forma1.Repository
 {
     public partial class Team : ITeamManageRacers
     {
+        /// <summary>
+        /// Új versenyző felvitele
+        /// </summary>
+        /// <param name="r">Új versenyző</param>
+        /// <exception cref="Exception">Két ugyanolyan versenyző nem adható a listához</exception>
         public void createRacer(Racer r)
         {
 
         }
 
+        /// <summary>
+        /// Töröl meglévő versenyzőt
+        /// </summary>
+        /// <param name="name">Törlendő versenyző neve</param>
+        /// <exception cref="Exception">A versenyző a csapatnak nem része, nem lehet törölni</exception>
+        /// <param name="newRacer"></param>
         public void deleteRacer(string name, Racer newRacer)
         {
 
         }
 
+        /// <summary>
+        /// Módosít meglévő versenyzőt
+        /// </summary>
+        /// <param name="name">Módosítandó versenyző</param>
+        /// <exception cref="Exception"></exception>
         public void updateRacer(string name)
         {
 
