e7c2a280ffddbdf0fc00fcfca4233a095034ca14
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-04 18:32:18 +0100

    Forma1 osztály init

---
+++ MVCDeveloping-master/Forma1/Forma1/Repository/F1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forma1.Repository
+{
+    public class F1
+    {
+        private List<Team> teams;
+
+        public F1()
+        {
+            teams = new List<Team>();
+        }
+    }
+}
