188d484459e774c11d4e5ff1808ca944e70b61f6
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-06 16:01:25 +0100

    Update EmailTarProjekt.java

--- EmailTarProjekt/src/emailtarprojekt/EmailTarProjekt.java
+++ EmailTarProjekt/src/emailtarprojekt/EmailTarProjekt.java
@@ -7,6 +7,12 @@ public class EmailTarProjekt {
      */
     public static void main(String[] args) {
 
-    }
+        String email = "Peti.vasvari.hu";
 
+        try {
+            EmailTar et = new EmailTar(email);
+        } catch (Exception e) {
+            System.out.println("Diák rossz e-mail cím miatt nem hozható létre!" + e.getMessage());
+        }
+    }
 }
