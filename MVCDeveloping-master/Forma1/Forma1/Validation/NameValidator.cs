a66df9b12ec31023718c960f3687aba2a913a35d
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-06 19:35:42 +0100

    validation() implementáció

--- MVCDeveloping-master/Forma1/Forma1/Validation/NameValidator.cs
+++ MVCDeveloping-master/Forma1/Forma1/Validation/NameValidator.cs
@@ -17,7 +17,40 @@ namespace Forma1.Validation
 
         public void validation()
         {
+            if (isEmpty())
+            {
+                throw new Exception("A név nem lehet üres!");
+            }
 
+            if (isFirstLetterNotUppercase())
+            {
+                throw new Exception("A név nagy kezdőbetűvel kell kezdődjön!");
+            }
+        }
+
+        private bool isFirstLetterNotUppercase()
+        {
+            if (name.Length == 0)
+            {
+                return true;
+            }
+
+            if (char.IsLower(name[0]))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool isEmpty()
+        {
+            if (name == string.Empty)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
