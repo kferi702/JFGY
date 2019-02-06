d4245c22f01148e426a70d74294192ea9edda155
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-06 15:53:33 +0100

    EmailTar konstruktor TDD fejlesztése

--- EmailTarProjekt/src/emailtarprojekt/EmailTar.java
+++ EmailTarProjekt/src/emailtarprojekt/EmailTar.java
@@ -4,7 +4,11 @@ public class EmailTar {
 
     private String email;
 
-    public EmailTar(String email) {
+    public EmailTar(String email) throws Exception {
+
+        if (email == "") {
+            throw new Exception("");
+        }
         this.email = email;
     }
 
