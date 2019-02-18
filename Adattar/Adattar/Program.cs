8521ccd220c0fa163d6fe82a93b51927935f1900
Bence Juhász <juhasz.bence@outlook.hu>
2019-02-18 18:12:29 +0100

    Update Program.cs

--- Adattar/Adattar/Program.cs
+++ Adattar/Adattar/Program.cs
@@ -8,8 +8,11 @@ namespace ListakbanLevoAdatokKezeleseOOPalapokon
 {
     class Program
     {
+        static List<int> szamok;
         static void Main(string[] args)
         {
+            VeletlenEgeszSzamok vesz = new VeletlenEgeszSzamok();
+            szamok = vesz.getSzamok();
         }
     }
 }
