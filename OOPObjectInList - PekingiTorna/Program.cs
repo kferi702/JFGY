337595bcddd2f224819c129ef089468becf5200a
Bence Juhász <juhasz.bence@outlook.hu>
2019-03-11 15:49:37 +0100

    Update Program.cs

--- OOPObjectInList - PekingiTorna/Program.cs
+++ OOPObjectInList - PekingiTorna/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OOPObjectInList___PekingiTorna.repository;
+using OOPObjectInList___PekingiTorna.model;
 
 namespace OOPObjectInList___PekingiTorna
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            PekingiTornaszTornaAdatokBeolvasasaFajlbol pt = new PekingiTornaszTornaAdatokBeolvasasaFajlbol();
+            pt.beolvas();
+            Console.ReadKey();
         }
     }
 }
