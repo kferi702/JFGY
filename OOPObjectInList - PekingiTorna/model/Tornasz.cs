2bbb6f4ea93af560673761d8bde856a97f924866
Bence Juhász <juhasz.bence@outlook.hu>
2019-03-11 15:26:05 +0100

    Peking torna feladat fájlbeolvasással. - init.

---
+++ OOPObjectInList - PekingiTorna/model/Tornasz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPObjectInList___PekingiTorna.model
+{
+    class Tornasz
+    {
+        public double Talaj { get; set; }
+        public double Lolenges { get; set; }
+        public double Gyuru { get; set; }
+        public double Nyujto { get; set; }
+        public double Korlat { get; set; }
+        public double Ugras { get; set; }
+
+        public Tornasz(double talaj, double lolenges, double gyuru, double nyujto, double korlat, double ugras)
+        {
+            Talaj = talaj;
+            Lolenges = lolenges;
+            Gyuru = gyuru;
+            Nyujto = nyujto;
+            Korlat = korlat;
+            Ugras = ugras;
+        }
+    }
+}
