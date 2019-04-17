553541744dc2e76d890f2d121a8047a0d09176c2
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2019-04-17 14:32:39 +0200

    validálás.

--- MVCDolgozat/Forma1/repository/TestData.cs
+++ MVCDolgozat/Forma1/repository/TestData.cs
@@ -9,20 +9,123 @@ namespace Forma1.repository
         {
             F1 f1 = new F1();
 
-            Racer r1 = new Racer(1, "Hamilton", 34, 245);
-            Racer r2 = new Racer(2, "Raikonen", 39, 45);
-            Racer r3 = new Racer(3, "Bottas", 27, 50);
-            Racer r4 = new Racer(4, "Vettel", 31, 182);
-            Racer r5 = new Racer(5, "Verstappen", 21, 70);
+            Racer r1 = null;
 
-            Racer r6 = new Racer(5, "fittipaldi", 88, 70);
-            Racer r7 = new Racer(5, "Verstappen", 21, 70);
-            Racer r8 = new Racer(5, "Csaba", 78, 10);
+            try
+            {
+                r1 = new Racer(1, "Hamilton", 34, 245);
+            }
+            catch (RacerException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
 
-            Team t1 = new Team("Mc Laren");
-            Team t2 = new Team("Ferrari");
+            Racer r2 = null;
+            try
+            {
+                r2 = new Racer(2, "Raikonen", 39, 45);
+            }
+            catch (RacerException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
 
-            Team t3 = new Team("trabant");
+            Racer r3 = null;
+
+            try
+            {
+                r3 = new Racer(3, "Bottas", 27, 50);
+            }
+            catch (RacerException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            Racer r4 = null;
+            try
+            {
+                r4 = new Racer(4, "Vettel", 31, 182);
+            }
+            catch (RacerException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            Racer r5 = null;
+
+            try
+            {
+                r5 = new Racer(5, "Verstappen", 21, 70);
+            }
+            catch (RacerException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            Racer r6 = null;
+            try
+            {
+                r6 = new Racer(5, "fittipaldi", 88, 70);
+            }
+            catch (RacerException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            Racer r7 = null;
+
+            try
+            {
+                r7 = new Racer(5, "Verstappen", 21, 70);
+            }
+            catch (RacerException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+
+            Racer r8 = null;
+
+            try
+            {
+                r8 = new Racer(5, "Csaba", 78, 10);
+            }
+            catch (RacerException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+
+            Team t1 = null;
+
+            try
+            {
+                t1 = new Team("Mc Laren");
+            }
+            catch (TeamException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            Team t2 = null;
+            try
+            {
+                t2 = new Team("Ferrari");
+            }
+            catch (TeamException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            Team t3 = null;
+            try
+            {
+                t3 = new Team("trabant");
+            }
+            catch (TeamException e)
+            {
+                Debug.WriteLine(e.Message);
+            }
 
             t1.addRacer(r1);
             t1.addRacer(r3);
